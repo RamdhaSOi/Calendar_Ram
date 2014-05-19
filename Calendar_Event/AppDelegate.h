b7d042df50736fe7a17e1e32e74afde4ce3a0f78
Ramdhas <ramdhasios@gmail.com>
2014-05-19 12:51:21 +0530

    Initial Commit

---
+++ Calendar_Event/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calendar_Event
+//
+//  Created by Ramdhas on May,19.
+//  Copyright (c) 2014 Ramdhas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calendar_Event/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calendar_Event
+//
+//  Created by Ramdhas on May,19.
+//  Copyright (c) 2014 Ramdhas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
